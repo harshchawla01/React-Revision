851f0ab40fc256ccd847ef2533c6d0038dcb6bb4
Harsh Chawla <chawlaharsh890@gmail.com>
2025-03-10 14:43:29 +0530

    Integrated zod for validation

--- 03-building-forms/src/components/Form.tsx
+++ 03-building-forms/src/components/Form.tsx
@@ -1,21 +1,28 @@
 import { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-// It is not necessary to define but by default compiler is not aware of out input fields if you want your from data to be shown in intelliSense, you can define an interface
-interface FormData {
-  name: string;
-  age: number;
-}
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }), // We can always pass a custom message if we don't want to use the default one
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .min(18, { message: "Age must be at least 18 years old" }),
+  // As there are multiple checks for single field, we can pass different error messages for each check
+}); // Passing configuration object (that represents shape of our form) as arguments to object method of zod
+
+// Removed the interface and using zod schema to validate the form
+// This ts type is similar to interface
+type FormData = z.infer<typeof schema>; // { name: string; age: number; }
 
 function Form() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>(); // nested destructuring
-  console.log(register("name")); // uses reference object so no rerendering is involved here
-  console.log(errors);
+    formState: { errors, isValid }, // nested destructuring
+  } = useForm<FormData>({ resolver: zodResolver(schema), mode: "onChange" });
 
+  // When using react-hook-form (useForm), you don't need to call event.preventDefault() manually. React Hook Form automatically prevents the default form submission behavior when using its handleSubmit function.
   const onSubmit = (data: FieldValues) => console.log("Data ", data);
 
   return (
@@ -26,20 +33,13 @@ function Form() {
           Name
         </label>
         <input
-          {...register("name", { required: true, minLength: 3 })}
+          //   {...register("name", { required: true, minLength: 3 })}
+          {...register("name")} // Don't mention validation rules here now. They are defined in the schema
           id="name"
           type="text"
           className="form-control"
         />
-        {errors.name?.type === "required" && (
-          <p className="text-danger">The name field is required.</p>
-        )}
-        {/* Optional chaining: '?' here signifies that it will check this condition only if errors has the property name */}
-        {errors.name?.type === "minLength" && (
-          <p className="text-danger">
-            The name must have a minimum length of 3
-          </p>
-        )}
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
 
       <div className="mb-3">
@@ -47,13 +47,14 @@ function Form() {
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })} // valueAsNumber is used to convert the value to number
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
