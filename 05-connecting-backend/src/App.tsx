c691697d1b290ad782803f2db2ad95c055221e26
Harsh Chawla <chawlaharsh890@gmail.com>
2025-03-11 14:31:25 +0530

    using axios for fetching backend with promise.then.catch

--- 05-connecting-backend/src/App.tsx
+++ 05-connecting-backend/src/App.tsx
@@ -1,19 +1,39 @@
-import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const connect = () => console.log("Connecting");
-const disconnect = () => console.log("Disconnecting");
+interface User {
+  // say we are just interested in first two properties only
+  id: number;
+  name: string;
+}
 
-const App = () => {
+interface Error {
+  message: string;
+}
+
+function App() {
+  // const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]); // We have to specify the type of state variable as an array of User objects because while fetching data, we are expecting an array of users
+  const [error, setError] = useState("");
   useEffect(() => {
-    connect(); // connecting / subscribing / showing modal
+    axios
+      // tell get that we are expecting an array of users
+      .get<User[]>("https://jsonplaceholder.typicode.com/userss")
+      // .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data)) // To have auto completion and type safety for properties of each user, we can define an interface for the data we are getting
+      .catch((err) => setError(err.message));
+  }, []);
 
-    // Did whatever we want
-
-    return () => disconnect(); // disconnecting / unsubscribing / hiding modal
-    // We know react renders our component twice in strict mode. Before rendering the second time. It will first unmount your component from the screen (basically the disconnect thing that we are doing here)
-  });
-
-  return <div></div>;
-};
+  return (
+    <>
+      <p className="text-danger">{error}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 export default App;
